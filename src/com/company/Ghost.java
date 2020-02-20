1eb6315e197cace031466ed1d28481b0bb6f458f
BenPerks340 <60626092+BenPerks340@users.noreply.github.com>
2020-02-20 12:53:42 -0700

    here is the Ghost class

---
+++ src/com/company/Ghost.java
@@ -0,0 +1,134 @@
+package com.company;
+import java.util.Random;
+import java.util.ArrayList;
+import java.lang.Math;
+public class Ghost {
+	
+//Instance Variables
+	public int xCoord;
+	public int yCoord;
+	public String name;
+	public String color;
+	
+//Default Constructor (creates a ghost at a random location)
+	public Ghost(){
+		//Getting the coordinates of the board and pacman
+		int lengthOfB = Board.getLength();
+		int heightOfB = Board.getHeight();
+		int pmX = PacMan.getXCoord();
+		int pmY = PacMan.getYCoord();
+		
+		//Assigns a random name and color to the ghost
+		Random rand = new Random();
+		this.name = names.get(rand.nextInt(names.size()));
+		Random rand2 = new Random();
+		this.color = colors.get(rand.nextInt(colors.size()));
+		
+		//Puts the ghost at a random length coordinate location where pacman is not
+		int minX = 0;
+		int maxX = lengthOfB;
+		int lCoord = pmX;
+		while (lCoord == pmX) {
+		lCoord = (int) (Math.random() * ((maxX - minX) + 1)) + minX; }
+		this.xCoord = lCoord;
+		
+		//Puts the ghost at a random height coordinate location where pacman is not
+		int minY = 0;
+		int maxY = heightOfB;
+		int hCoord = pmX;
+		while (hCoord == pmY) {
+		hCoord = (int) (Math.random() * ((maxY - minY) + 1)) + minY; }
+		this.yCoord = hCoord;
+	
+	}
+	
+//Constructor (initializing instance variables)
+	public Ghost(int xCoord, int yCoord, String name, String color) {
+		if (xCoord >= 0) this.xCoord = xCoord;
+		if (yCoord >= 0) this.yCoord = yCoord;
+		this.name = name;
+		this.color = color;
+	}
+	
+//Copy constructor
+	public Ghost(Ghost G) {
+		xCoord = G.xCoord;
+		yCoord = G.yCoord;
+		name = G.name;
+		color = G.color;
+	}
+	
+//Method Getter xCoord
+	public int getXCoord() {
+		return this.xCoord;
+	}
+
+//Method Getter yCoord
+	public int getYCoord() {
+		return this.yCoord;
+	}
+
+//Method Getter name
+	public String getName() {
+		return this.name;
+	}
+	
+//Method Getter color
+	public String getColour() {
+		return this.color;	
+	}
+	
+//Method Setter xCoord
+	public void setXCoord(int xCoord) {
+		if (xCoord >= 0) {
+		this.xCoord = xCoord; }
+	}
+	
+//Method Setter yCoord
+	public void setYCoord(int yCoord) {
+		if (yCoord >= 0) {
+		this.yCoord = yCoord; }
+	}	
+	
+//Method Setter name
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+//Method Setter color
+	public void setColour(String colour) {
+		this.color = colour;
+	}
+	
+//Method moveUP
+	public void moveUp(int numCoord) {
+		if ((this.yCoord =- numCoord) >= 0) {
+			this.yCoord =- numCoord; }
+	}
+	
+//Method moveDown 
+	public void moveDown(int numCoord) {
+		int heightOfB = Board.getHeight();
+		if ((this.yCoord =+ numCoord) <= heightOfB) {
+		this.yCoord =+ numCoord; }
+	}
+	
+//Method moveRight (NEED TO CHECK FOR MAX IT CAN GO, DEPENDANT ON SIZE OF BOARD)
+	public void moveRight(int numCoord) {
+		int lengthOfB = Board.getLength();
+		if ((this.xCoord =+ numCoord) <= lengthOfB) {
+		this.xCoord =+ numCoord; }
+	}
+	
+//Method moveLeft
+	public void moveLeft(int numCoord) {
+		if ((this.xCoord =- numCoord) >= 0) {
+			this.xCoord =- numCoord; }
+	}
+
+}
+
+
+
+
+
