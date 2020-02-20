5e605094c053545f740b52162e6e62b4ea5752ae
ArmaanMohan <50090831+ArmaanMohan@users.noreply.github.com>
2020-02-20 11:47:17 -0700

    Created and implemented movePacMan method which takes user input to move player's PacMan character.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,31 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello World!");
+	    PacMan player = new PacMan();
+        movePacMan(player);
+    }
+
+    public static void movePacMan(PacMan player){
+        Scanner userInput = new Scanner(System.in);
+        String moveToDo;
+        do {
+            System.out.print("Type the move for PacMan (end to end game): ");
+            moveToDo = userInput.next();
+            moveToDo = moveToDo.toLowerCase();
+            if (moveToDo.contentEquals("up"))
+                player.moveUp();
+            else if (moveToDo.contentEquals("down"))
+                player.moveDown();
+            else if (moveToDo.contentEquals("left"))
+                player.moveLeft();
+            else if (moveToDo.contentEquals("right"))
+                player.moveRight();
+            System.out.println("Player is at x = " + player.getXCoord() + " and y = " + player.getYCoord());
+        } while (!moveToDo.contentEquals("end"));
+
+
     }
 }
